2cacb794dceb66659ae7b40f53c66ad54ca58df6
vise <150336391+vise07@users.noreply.github.com>
2025-10-17 22:47:00 +0200

    Delete DB_SQL/pag_116/modifica_tabella.sql

--- DB_SQL/pag_116/modifica_tabella.sql
+++
@@ -1,6 +0,0 @@
-alter table autori
-  change column cf codice_fiscale int not null unique,
-  add column indirizzo varchar(40),
-  add column cap varchar(5),
-  change column cognome cognome varchar(50) not null,
-  change column nome nome varchar(50) not null;
